b2e22e24e96b8ad937d1e4e23502c8fdd1bbec77
202230125 <sumun4022@naver.com>
2024-09-19 10:25:44 +0900

    v0.8

--- week04/week04_web01.py
+++ week04/week04_web01.py
@@ -7,4 +7,12 @@ api = "https://www.kma.go.kr/weather/forecast/mid-term-rss3.jsp"
 station_id = input("지역 코드를 입력하세요 : ")
 values = {'stnId':station_id}
 
-url = api + '?' +
+url = api + '?' + str(values)
+print(url)
+
+
+"""
+https://www.kma.go.kr/weather/forecast/mid-term-rss3.jsp?{'stnId': '184'}
+
+Process finished with exit code 0
+"""
