26dcec7c474448464994db2f1ee6f282adf6b8d2
202230125 <sumun4022@naver.com>
2024-09-19 10:26:01 +0900

    v0.9

--- week04/week04_web01.py
+++ week04/week04_web01.py
@@ -7,12 +7,13 @@ api = "https://www.kma.go.kr/weather/forecast/mid-term-rss3.jsp"
 station_id = input("지역 코드를 입력하세요 : ")
 values = {'stnId':station_id}
 
-url = api + '?' + str(values)
+url = api + '?' + urllib.parse.urlencode(values)
 print(url)
 
 
 """
 https://www.kma.go.kr/weather/forecast/mid-term-rss3.jsp?{'stnId': '184'}
+https://www.kma.go.kr/weather/forecast/mid-term-rss3.jsp?stnId=184
 
 Process finished with exit code 0
 """
