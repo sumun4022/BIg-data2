58a45547991bf7219fe0c2e814447f8e1d05a049
202230125 <sumun4022@naver.com>
2024-10-31 12:14:33 +0900

    v2.4

--- Week10/week10_dataset.py
+++ Week10/week10_dataset.py
@@ -1,8 +1,9 @@
 import seaborn as sns
 import numpy as np
 
-print(sns.get_dataset_names())
-# titanic = sns.load_Dateset('titanic')
-
-mpg = sns.load_dataset('mpg')
-print(mpg.head().sort_values('mpg'))
+titanic = sns.load_dataset('titanic')
+# print(titanic.describe())
+# print(titanic.head())
+# print(titanic.tail(10))
+print(titanic.info())
+# print(titanic['embarked'], titanic['deck'], titanic['class'])
