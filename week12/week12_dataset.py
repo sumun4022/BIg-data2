c40f8f7d2a2227f67e2500123a5214c455e53aea
202230125 <sumun4022@naver.com>
2024-11-14 12:07:41 +0900

    v0.1 Logicist

--- week12/week12_dataset.py
+++ week12/week12_dataset.py
@@ -1,6 +1,6 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.neighbors import KNeighborsRegressor
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
 
@@ -9,12 +9,12 @@ median_age = titanic['age'].median()
 titanic_fill = titanic.fillna({'age': median_age})
 
 X = titanic_fill[['age']]  #독립 변수
-y = titanic_fill[['survived']] #독립 변수
+y = titanic_fill['survived'] #독립 변수
 
 X_trian, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # 모델 선택
-model = KNeighborsRegressor(n_neighbors=5)
+model = LogisticRegression()
 
 # 모델 학습
 model.fit(X_trian, y_train)
@@ -25,7 +25,7 @@ y_pred = model.predict(X_test)
 # 시각화
 plt.figure(figsize=(10,5))
 plt.scatter(X_test, y_test, color='blue',label='Actual')
-plt.scatter(X_test, y_pred, color='red',label='Predicted')
+plt.scatter(X_test, y_pred, color='red',label='Predicted', alpha=0.2)
 plt.title('Linear Regression: Real vs Predict')
 plt.xlabel('Age')
 plt.ylabel('survival')
