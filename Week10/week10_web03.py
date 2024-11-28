292aa780cb963ac02f208073bc6c0519a811a393
202230125 <sumun4022@naver.com>
2024-11-28 12:11:43 +0900

    v4.0

--- Week10/week10_web03.py
+++ Week10/week10_web03.py
@@ -11,10 +11,10 @@ shops = []
 for i in range(1, 51):
     url = f"https://www.hollys.co.kr/store/korea/korStore2.do?pageNo={i}&sido=&gugun=&store="
     # print(url)
-    page = urllib.request.urlopen(url)
-    soup = BeautifulSoup(page, "html.parser")
-    tbody = soup.find('tbody')
-    trs = tbody.find_all('tr')
+    page = urllib.request.urlopen(url)          #특정 웹 페이지의 HTML을 가져온 후 page 변수에 저장
+    soup = BeautifulSoup(page, "html.parser")       #page에 저장된 html을 파이썬으로 바꾸고 soup 변수에 저장
+    tbody = soup.find('tbody')      #파싱된 html 객체에서 body태그를 찾고 body태그 안에 있는 모든 코드를 tbody 변수에 저장
+    trs = tbody.find_all('tr')      #tbody안에 있는 코드에서 tr태그를 찾고 그 안에 있는 모든 코드를 리스트로 반환 후에 trs에 저장    
 
     for tr in trs:
         tds = tr.find_all('td')
@@ -27,3 +27,6 @@ for i in range(1, 51):
 # print(shops)
 hollys_df = pd.DataFrame(shops, columns = ('매장 이름', '주소', '전화 번호', '일시'))
 hollys_df.to_csv('할리스 매장 목록.csv', mode='w', encoding='cp949')
+
+# 인코딩 방식 중에서 메모장이나 엑셀에서는 cp949로 인코딩을 해야한다.
+# 기본은 uft-8로 파이참에서 볼 수 있다.
